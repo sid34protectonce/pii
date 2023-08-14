30ea841207b6bdd277c8f1a4ae3899a78132cff5
sid34protectonce <siddharth.s@protectonce.com>
2023-08-14 15:03:35 +0530

    Co-authored-by: arpitsarap <arpitsarap@users.noreply.github.com>

--- tests/test.ts
+++ tests/test.ts
@@ -32,87 +32,237 @@ describe("Testing number of pii", () => {
                 }
             }
             else if (res.name === "User Email"){
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "IP Address"){
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "Address") {
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "Browser Name"){
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "OS name"){
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "Indian Phone Number") {
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "International Phone Number"){
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
 
             else if (res.name === "Bank Account Number") {
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (
               res.name ===
               "Credit Card Number (VISA, MASTERCARD, AMERICAN EXPRESS, Diners Club, Diners Club, JCB"
             ){
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
               
             else if (res.name === "Debit Card") {
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "MAC Address") {
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "IMSI") {
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "Date") {
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "Zip code"){
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "SSN"){
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "Driving License") {
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "Gender") {
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "Internation Passport number"){
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
      
             else if (res.name === "financial_count") {
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "Geolocation (Latitude)"){
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
 
             else if (res.name === "URL"){
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
-            else if (res.name === "Indian Vechical Registration Number"){
-
+            else if (res.name === "Indian Vehical Registration Number"){
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
               
             else if (res.name === "IMEI") {
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "Addhar Card") {
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
             else if (res.name === "PAN CARD") {
-
+                if(res.count === 0){
+                    console.log(`Passed ${res.name} count test`);
+                }
+                else{
+                    console.log(`Failed ${res.name} count test`);
+                    exit(1);
+                }
             }
         }
     })
